c309e14cf4c92f4337168cb73ae34cf0c68aa9bd
meowmeow321 <prakhardwivedi.cse18@itbhu.ac.in>
2021-04-08 20:22:27 +0530

    Added failing case for cut to tests.

--- main.py
+++ main.py
@@ -12,15 +12,16 @@ script_dir = os.path.dirname(__file__)
 # TESTING for optimal query set
 # hand-made cases
 print("TESTING ON HAND-MADE CASES...")
-for i in range(1, 8):
+for i in range(1, 9):
     rel_path = "tests/test" + str(i) + ".txt"
     abs_file_path = os.path.join(script_dir, rel_path)
     g = UncertainGraph()
     g.buildFromFile(abs_file_path)
+    # print(g)
     optimalSet = optimalQuerySet(g)
     assert checkOPT(g, optimalSet)
-    # print(len(optimalSet))
-    # print(optimalSet)
+    print(len(optimalSet))
+    print(optimalSet)
     algoCycleObject = CycleModel(g)
     algoCycleQuery = algoCycleObject.Q
     # print("Algo Query:", algoCycleQuery)
@@ -37,8 +38,8 @@ for i in range(1, 8):
 
 # randomly generated cases
 testcases = 0
-testcases = int(input("Enter number of cases to generate and test: "))
-debug = input("Do you want to print generator arguments? [Y/N]: ")
+# testcases = int(input("Enter number of cases to generate and test: "))
+# debug = input("Do you want to print generator arguments? [Y/N]: ")
 for i in range(1, testcases + 1):
     generatorObject = GraphGenerator(debug == 'Y')
     g = generatorObject.constructGraph(random.randint(12, 15), random.randint(
